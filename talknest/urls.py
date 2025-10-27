e6b1dfa5d50eae7e918310b033ec4d19de67609b
Valt <Dangthanhnhanvalt@gmail.com>
2025-10-27 12:58:32 +0700

    fix: remove secret from settings

--- talknest/urls.py
+++ talknest/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 from talknest.views import home
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('register/', views.register, name='register'),
+    path('accounts/', include('allauth.urls')),
+    path('login/', views.login, name='login'),
 ]
