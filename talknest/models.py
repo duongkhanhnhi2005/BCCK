b3c9fe35995ffa3b5816338e34618b538b56c373
duongkhanhnhi2005 <duongkhanhnhi2005@gmail.com>
2025-10-28 10:05:25 +0700

    Lưu lại thay đổi local trước khi pull

--- talknest/models.py
+++ talknest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Topic(models.Model):
+    topic_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
--- talknest/urls.py
+++ talknest/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from talknest.views import home
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('search/', views.search, name='search'),
 ]
