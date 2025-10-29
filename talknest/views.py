98f5247c6486365c22b33b3f26e99079c10d5e70
thuhienqn1306-eng <thuhienqn1306@gmail.com>
2025-10-29 18:11:43 +0700

    Update TalkNest Django project

--- talknest/views.py
+++ talknest/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, 'home_user.html')
+    else:
+        return render(request, 'home_guest.html')
