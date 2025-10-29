8d2dd2e90d27a1e9ee5de4fde46a1a69e6e81a97
duongkhanhnhi2005 <duongkhanhnhi2005@gmail.com>
2025-10-29 15:40:35 +0700

    Update views.py

--- talknest/views.py
+++ talknest/views.py
@@ -1,8 +1,53 @@
 from django.shortcuts import render
+from .models import Topic, Post
+from django.db.models import Q
 
 def home(request):
-    return render(request, "home.html")
+    topics = Topic.objects.all()
+    return render(request, "home.html",{'topics': topics})
+
+def search(request):
+    keyword = request.GET.get('keyword', '')
+    user = request.GET.get('user', '')
+    topic_id = request.GET.get('topic', '')
+    before = request.GET.get('before', '')
+    after = request.GET.get('after', '')
+
+    posts = None  # mặc định chưa tìm
+
+    # chỉ thực hiện tìm kiếm nếu có ít nhất 1 tham số GET
+    if any([keyword, user, topic_id not in [None, '', 'all'], before, after]):
+        posts = Post.objects.all()
+
+    if keyword:
+        posts = posts.filter(Q(title__icontains=keyword) | Q(content__icontains=keyword))
+
+    if user:
+        posts = posts.filter(author__username__icontains=user)
+
+    if topic_id and topic_id != 'all':
+        posts = posts.filter(topic_id=topic_id)
+
+    if before:
+        posts = posts.filter(created_at__lte=before)
+
+    if after:
+        posts = posts.filter(created_at__gte=after)
+
+    topics = Topic.objects.all()
+
+    return render(request, 'search.html', {
+        'posts': posts,
+        'topics': topics,
+        'keyword': keyword,
+        'user': user,
+        'before': before,
+        'after': after,
+        'topic_id': topic_id,
+    })
+
 def register(request):
     return render(request, "register.html")
 def login(request):
     return render(request, "login.html")
+
